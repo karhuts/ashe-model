0dc9f19a34527236abdd7b0b74843c9c2cb98a83
Min <294953530@qq.com>
2023-07-26 18:25:03 +0800

    add im sql struct

--- models/message.go
+++ models/message.go
@@ -6,7 +6,7 @@ import "time"
 type Message struct {
 	ID         int64     `gorm:"column:id" db:"id" json:"id"`                            //  自增主键
 	UserId     int64     `gorm:"column:user_id" db:"user_id" json:"user_id"`             //  所属类型的id
-	RequestId  int64     `gorm:"column:request_id" db:"request_id" json:"request_id"`    //  请求id
+	RequestId  string    `gorm:"column:request_id" db:"request_id" json:"request_id"`    //  请求id
 	Code       int64     `gorm:"column:code" db:"code" json:"code"`                      //  消息类型
 	Content    string    `gorm:"column:content" db:"content" json:"content"`             //  消息内容
 	Seq        int64     `gorm:"column:seq" db:"seq" json:"seq"`                         //  消息序列号
